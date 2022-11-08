ee8d6bb415f928cbaf8717c0ca195caacb1fcd6a
vasil-yordanov14 <vasil_sh@abv.bg>
2022-11-08 23:12:00 +0200

    status codes

--- controllers/main.js
+++ controllers/main.js
@@ -1,11 +1,11 @@
 const jwt = require("jsonwebtoken");
-const CustomAPIError = require("../errors/custom-error");
+const { BadRequestError } = require("../errors");
 
 const login = async (req, res) => {
   const { username, password } = req.body;
 
   if (!username || !password) {
-    throw new CustomAPIError("Please provide email and password", 400);
+    throw new BadRequestError("Please provide email and password");
   }
   const id = new Date().getDate();
   const token = jwt.sign({ id, username }, process.env.JWT_SECRET, {
