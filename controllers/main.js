89f3e2d95d75fdcca11132e9899b822e8660488b
vasil-yordanov14 <vasil_sh@abv.bg>
2022-11-08 20:54:18 +0200

    verify token

--- controllers/main.js
+++ controllers/main.js
@@ -23,12 +23,18 @@ const dashboard = async (req, res) => {
   }
 
   const token = authHeader.split(" ")[1];
-  console.log(token);
-  const luckyNumber = Math.floor(Math.random() * 100);
-  res.status(200).json({
-    msg: "Hello John Doe",
-    secret: `Here is your code: ${luckyNumber}`,
-  });
+
+  try {
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+
+    const luckyNumber = Math.floor(Math.random() * 100);
+    res.status(200).json({
+      msg: `Hello ${decoded.username}`,
+      secret: `Here is your authorized data. Your lucky number is: ${luckyNumber}`,
+    });
+  } catch (error) {
+    throw new CustomAPIError("Not authorized to access this route", 401);
+  }
 };
 
 module.exports = {
